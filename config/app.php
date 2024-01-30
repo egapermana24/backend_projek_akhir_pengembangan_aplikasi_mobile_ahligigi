b302a4f0268b7cf872714aaddfff5d75cb870030
Ega Permana <permanaega677@gmail.com>
2024-01-30 10:41:47 +0700

    mengubah href css

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://ahligigi-f9463a56ce18.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL'),
 
